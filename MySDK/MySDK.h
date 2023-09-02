38928ae3e7136a85347fbd394ede9a542855d8e8
Afzaal Ahmad <ch.afzaalahmad@gmail.com>
2023-09-02 18:24:09 -0500

    Initial Commit

---
+++ MySDK/MySDK.h
@@ -0,0 +1,18 @@
+//
+//  MySDK.h
+//  MySDK
+//
+//  Created by Afzaal Ahmad on 9/2/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MySDK.
+FOUNDATION_EXPORT double MySDKVersionNumber;
+
+//! Project version string for MySDK.
+FOUNDATION_EXPORT const unsigned char MySDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MySDK/PublicHeader.h>
+
+
